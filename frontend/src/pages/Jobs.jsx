8f9f2c371a9d4429f6d9ad2c019c1107d53de5a0
Bellam Guru Srikar <srikarsri2004@gmail.com>
2026-06-04 16:01:15 +0530

    Add job status update functionality

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -29,6 +29,26 @@ function Jobs(){
             console.log(error);
         }
     };
+    
+    const updateStatus = async (id,newStatus)=>{
+        try{
+            await axios.patch(
+                `http://127.0.0.1:8000/api/jobs/${id}/`,
+                {
+                    status: newStatus
+                }
+            );
+            setJobs(
+                jobs.map((job)=>
+                    job.id === id
+                    ?{ ...job,status: newStatus}
+                    : job
+                )
+            );
+        } catch (error){
+            console.log(error);
+        }
+    };
     return (
         <div>
             <h1>My Applications</h1>
@@ -50,7 +70,15 @@ function Jobs(){
                         <td>{job.company_name}</td>
                         <td>{job.job_title}</td>
                         <td>{job.location}</td>
-                        <td>{job.status}</td>
+                        <td>
+                            <select value={job.status} 
+                            onChange={(e)=> updateStatus(job.id,e.target.value)}>
+                                <option value="Applied">Applied</option>
+                                <option value="Interview">Interview</option>
+                                <option value="Rejected">Rejected</option>
+                                <option value="Offer">Offer</option>
+                            </select>
+                        </td>
                         <td><button onClick={()=>deleteJob(job.id)}>
                                 Delete
                             </button>
