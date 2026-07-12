17d86cb27bf617ebdf860cbbae94ce256904145c
Bellam Guru Srikar <srikarsri2004@gmail.com>
2026-07-12 15:20:53 +0530

    Temporarily fix ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,10 +27,11 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost",
-).split(",")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "job-tracker-backend-tpny.onrender.com",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.onrender.com",
