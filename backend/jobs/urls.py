811202c4134c3d95e142a4f466bb33c9ed7012d2
Bellam Guru Srikar <srikarsri2004@gmail.com>
2026-07-14 21:14:25 +0530

    Integrate Cloudinary for resume uploads

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -3,13 +3,10 @@ from .views import (
     JobApplicationListCreateView, 
     JobApplicationDetailView,
     RegisterView, 
-    test_resume,media_debug,
     )
 
 urlpatterns = [
     path('jobs/', JobApplicationListCreateView.as_view()),
     path('jobs/<int:pk>/', JobApplicationDetailView.as_view()),
     path('register/',RegisterView.as_view()),
-    path("test-resume/", test_resume),
-    path("media-debug/", media_debug),
 ]
