c31f92e89e3788ffbb8aee71c30e23218a703136
Bellam Guru Srikar <srikarsri2004@gmail.com>
2026-07-14 18:40:47 +0530

    remove if block in config/urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -37,8 +37,3 @@ urlpatterns += static(
     document_root=settings.MEDIA_ROOT
 )
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
